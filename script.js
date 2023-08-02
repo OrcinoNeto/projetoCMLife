41bbe354549a66c005418472105f700b6bc83759
OrcinoNeto <129555010+OrcinoNeto@users.noreply.github.com>
2023-08-02 14:43:01 -0300

    Update script.js
    Atualização.

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-    
-    
-    return rect.top <= windowHeight * 0.6;
-  }
+  const rect = el.getBoundingClientRect();
+  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
   
-  function onScroll() {
-    const sections = document.querySelectorAll('.fade-in-left, .fade-in-right, .fade-in-topo, .fade-in-botton');
-    sections.forEach(section => {
-      if (isElementInViewport(section)) {
-        section.classList.add('fade-in');
-      }
-    });
-  }
-  
-  
-  document.addEventListener('DOMContentLoaded', onScroll);
-  
-  document.addEventListener('scroll', onScroll);
+  return rect.bottom >= 0 && rect.top <= windowHeight;
+}
+
+function onScroll() {
+  const sections = document.querySelectorAll('.fade-in-left, .fade-in-right, .fade-in-topo, .fade-in-botton');
+  sections.forEach(section => {
+    if (isElementInViewport(section)) {
+      section.classList.add('fade-in');
+      section.classList.remove('fade-out');
+    } else {
+      section.classList.remove('fade-in');
+      section.classList.add('fade-out');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', onScroll);
+document.addEventListener('scroll', onScroll);
